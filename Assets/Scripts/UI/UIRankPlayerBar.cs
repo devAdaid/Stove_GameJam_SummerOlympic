23664c69ea826ac67cd8948b24ec763bc9716f59
joyounwoo <joyounwoobasedata@gmail.com>
2021-08-29 08:34:06 +0900

    엔딩 기능 제작, 리소스 적용 및 이펙트

--- Assets/Scripts/UI/UIRankPlayerBar.cs
+++ Assets/Scripts/UI/UIRankPlayerBar.cs
@@ -8,7 +8,16 @@ public class UIRankPlayerBar : MonoBehaviour
     public Image medalImage;
     public Image nationFlagImage;
 
-    public Text nationCodeText;
     public Text nationKoreanNameText;
     public Text playerNameText;
+
+    public void UpdateUI(string playterName, string nationName)
+    {
+        playerNameText.text = playterName;
+
+        int index = NationDatas.I.nationDatas.FindIndex(x => x.Name == nationName);
+
+        nationFlagImage.sprite = NationDatas.I.nationDatas[index].FlagSprite;
+        nationKoreanNameText.text = NationDatas.I.nationDatas[index].Name;
+    }
 }
