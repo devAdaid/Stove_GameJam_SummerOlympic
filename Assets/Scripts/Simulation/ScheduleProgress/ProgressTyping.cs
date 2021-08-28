2f1d51467d1f1bf0a79c5180e8a33353065577cd
pearl0408 <roskfl000408@naver.com>
2021-08-29 01:33:57 +0900

    일정 출력 수정
    수정중.

--- Assets/Scripts/Simulation/ScheduleProgress/ProgressTyping.cs
+++ Assets/Scripts/Simulation/ScheduleProgress/ProgressTyping.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -98,3 +98,4 @@ public class ProgressTyping : MonoBehaviour
         }
     }
 }
+*/
