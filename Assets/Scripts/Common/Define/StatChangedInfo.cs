eb7f82a8c1d42e1a4122831cf2c5c1522905c53e
devAdaid <ahyeong2533@gmail.com>
2021-08-29 01:30:05 +0900

    일정 요청 데이터 수정

--- Assets/Scripts/Common/Define/StatChangedInfo.cs
+++ Assets/Scripts/Common/Define/StatChangedInfo.cs
@@ -1,17 +1,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StatChangedInfo
-{
-    public StatType StatType;
-    public int DiffValue;
-    public int Value => Mathf.Abs(DiffValue);
-    public bool IsIncreased => DiffValue > 0;
-}
-
 public class 진주님_클래스
 {
-    public void ProcessSchedule(List<StatChangedInfo> statChangedInfo, string scheduleName)
+    public void ProcessSchedule(ProcessScheduleRequestData requestData, string scheduleName, int day)
     {
         // 일정을 증가시키고 타이핑 연출을 보여줌....
     }
