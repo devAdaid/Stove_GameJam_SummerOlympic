8222a290969719c2ebbadac6e9144455a5c2a7fb
devAdaid <ahyeong2533@gmail.com>
2021-08-29 15:01:58 +0900

    수정6

--- Assets/Scripts/Common/Define/Constant.cs
+++ Assets/Scripts/Common/Define/Constant.cs
@@ -5,7 +5,7 @@ public static class Constant
     public static readonly int SWIMSTAT_MAX = 1500;
     public static readonly int INITIAL_MONTH = 8;
     public static readonly int GOLD_DELIVERED_AT_MONTH = 50000;
-    public static readonly int STAMINA_DELIVERED_AT_MONTH = 300;
+    public static readonly int STAMINA_DELIVERED_AT_MONTH = 0;
     public static readonly int DAY_PER_WEEK_COUNT = 7;
     public static readonly int WEEK_PER_MONTH_COUNT = 4;
     public static readonly int MONTH_COUNT = 12;
